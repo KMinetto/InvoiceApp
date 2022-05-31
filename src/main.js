f34ae5145418d078ac8c127c3ea20db100b474a6
Kilian MINETTO <killian.minetto@gmail.com>
2022-06-01 00:27:00 +0200

    Adding some dependencies

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+// Import icons needed here
+// import { faAngleDoubleUp } from "@fortawesome/free-solid-svg-icons";
 import App from "./App.vue";
+import "./assets/tailwind.css";
 
-createApp(App).mount("#app");
+// Add icons here
+library.add(fas);
+createApp(App).component("fa", FontAwesomeIcon).mount("#app");
