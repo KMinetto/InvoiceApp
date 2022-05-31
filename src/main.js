17f54cd5051b24da556a2c36b6a2f2b6a24f6ef4
Kilian MINETTO <killian.minetto@gmail.com>
2022-06-01 01:13:29 +0200

    Setting the project structure

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 // Import icons needed here
 // import { faAngleDoubleUp } from "@fortawesome/free-solid-svg-icons";
 import App from "./App.vue";
+import router from "./router/index";
+import store from "./store/index";
 import "./assets/tailwind.css";
 
 // Add icons here
 library.add(fas);
-createApp(App).component("fa", FontAwesomeIcon).mount("#app");
+createApp(App)
+  .component("fa", FontAwesomeIcon)
+  .use(store)
+  .use(router)
+  .mount("#app");
