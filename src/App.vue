096a34414f4713343032145ac2af3b17c7a09b1f
Kilian MINETTO <killian.minetto@gmail.com>
2022-06-01 13:59:37 +0200

    Creating Navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
+<!--suppress ALL -->
 <template>
-  <router-view></router-view>
+  <div>
+    <div
+      v-if="!mobile"
+      class="app flex flex-col lg:flex-row bg-darkPurple min-h-screen"
+    >
+      <Navigation></Navigation>
+      <div class="app-content relative flex flex-1">
+        <router-view></router-view>
+      </div>
+    </div>
+    <div
+      v-else
+      class="mobile-message text-center text-white flex flex-col justify-center align-middle h-screen bg-darkPurple"
+    >
+      <h2 class="text-2xl font-bold">
+        Désolé, cette application n'est pas supporté sur mobile.
+      </h2>
+      <p class="mt-4">
+        Pour utiliser cette application, vous devriez utiliser un ordinateur.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 import "./assets/tailwind.css";
+import Navigation from "./components/Navigation";
 
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      mobile: null,
+    };
+  },
+  components: {
+    Navigation,
+  },
+  created() {
+    this.checkScreen();
+    window.addEventListener("resize", this.checkScreen);
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
 
-$white-color: #fefefe;
-$draft-color: #dfe3fa;
-$dark-purple: #252945;
-$red: #ec5757;
-$purple: #7c5dfa;
-$green: #33d69f;
-$orange: #ff8f00;
+.app {
+  background-color: #141625;
+  .app-content {
+    padding: 0 20px;
+  }
+}
 
 button,
 .button {
@@ -30,23 +73,7 @@ button,
   border-radius: 30px;
   font-size: 12px;
   margin-right: 8px;
-  color: $white-color;
-}
-
-.dark-purple {
-  background-color: $dark-purple;
-}
-.purple {
-  background-color: $purple;
-}
-.red {
-  background-color: $red;
-}
-.orange {
-  background-color: $orange;
-}
-.green {
-  background-color: $green;
+  color: #fefefe;
 }
 
 // Status button Styling
@@ -68,25 +95,22 @@ button,
 
 .paid {
   &::before {
-    background-color: $green;
+    background-color: #33d69f;
   }
-  color: $green;
   background-color: rgba(51, 214, 160, 0.1);
 }
 
 .pending {
   &::before {
-    background-color: $orange;
+    background-color: #ff8f00;
   }
-  color: $orange;
   background-color: rgba(255, 145, 0, 0.1);
 }
 
 .draft {
   &::before {
-    background-color: $draft-color;
+    background-color: #dfe3fa;
   }
-  color: $draft-color;
   background-color: rgba(223, 227, 250, 0.1);
 }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <header
+    class="flex flex-row lg:flex-col lg:min-h-full lg:min-w-large lg:rounded-tr-large lg:rounded-br-large bg-darkPurple z-[99]"
+  >
+    <div
+      class="flex justify-center rounded-tr-large rounded-br-large lg:w-full p-6 bg-purple"
+    >
+      <img
+        class="w-auto h-7.5"
+        src="@/assets/images/file-invoice-dollar-solid.png"
+        alt=""
+      />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Navigation",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: #1e2139;
+}
+</style>
